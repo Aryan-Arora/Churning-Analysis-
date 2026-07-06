3263da7542b913d5265136bab04aa7b16fb41af0
Aryan <72548611+Aryan-Arora@users.noreply.github.com>
2026-07-06 15:56:03 +0530

    Add files via upload

---
+++ Gradient_Boosting.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import accuracy_score
+
+df=pd.read_csv("Churn_Modelling.csv")
+df=df.drop(columns=["RowNumber","CustomerId","Surname"])
+
+X=df.drop(columns=["Exited"])
+Y=df["Exited"]
+
+X_train, X_test, Y_train, Y_test=train_test_split(X,Y,test_size=0.2,random_state=42)
+
+le=LabelEncoder()
+X_train["Gender"]=le.fit_transform(X_train["Gender"])
+X_test["Gender"]=le.transform((X_test["Gender"]))
+
+X_train=pd.get_dummies(X_train, columns=["Geography"],drop_first=True)
+X_test=pd.get_dummies(X_test, columns=["Geography"],drop_first=True)
+
+scaler=StandardScaler()
+X_train=scaler.fit_transform(X_train)
+X_test=scaler.transform(X_test)
+
+gb_model = GradientBoostingClassifier(n_estimators=10000, learning_rate=0.1, random_state=42)
+gb_model.fit(X_train, Y_train)
+y_pred_gb = gb_model.predict(X_test)
+print(accuracy_score(Y_test, y_pred_gb))
